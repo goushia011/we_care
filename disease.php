27dbd6f46c83038d72ff4942b36b36dc6f072a38
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2023-03-18 13:20:12 +0530

    dash partial and symptoms checkboxes

---
+++ disease.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+ob_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+  <link rel="stylesheet" href="css/home.css">
+  <title>wecare</title>
+</head>
+
+<body>
+  <?php include "./layout/dash.php" ?>
+  <div class="main">
+    <h1> Disease Prediction system</h1>
+    <section>
+      <?php $symptoms_str = file_get_contents("data/symptoms.json") ?>
+      <?php $symptoms = json_decode($symptoms_str) ?>
+      <div class="symptoms-container">
+        <?php foreach ($symptoms as $key => $value) { ?>
+          <div>
+            <label for="<?php echo $key ?>">
+              <input type="checkbox" name="symptoms[]" id="<?php echo $key ?>">
+              <?php echo $value ?>
+            </label>
+          </div>
+        <?php } ?>
+      </div>
+      <div class="submit">
+        <input type="submit">
+        <input type="text">
+      </div>
+    </section>
+  </div>
+</body>
