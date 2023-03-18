2514d44953784c368e943adb302dde653c100a6b
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2023-03-18 14:02:47 +0530

    refactored symptoms and disease page

--- disease.php
+++ disease.php
@@ -9,9 +9,10 @@ ob_start();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
   <link rel="stylesheet" href="css/home.css">
+  
   <title>wecare</title>
 </head>
 
@@ -22,17 +23,19 @@ ob_start();
     <section>
       <?php $symptoms_str = file_get_contents("data/symptoms.json") ?>
       <?php $symptoms = json_decode($symptoms_str) ?>
-      <div class="symptoms-container">
-        <?php foreach ($symptoms as $key => $value) { ?>
-          <div>
-            <label for="<?php echo $key ?>">
-              <input type="checkbox" name="symptoms[]" id="<?php echo $key ?>">
-              <?php echo $value ?>
-            </label>
-          </div>
-        <?php } ?>
+      <div style="display: flex; justify-content: center;">
+        <div class="symptoms-container">
+          <?php foreach ($symptoms as $key => $value) { ?>
+            <div>
+              <label for="<?php echo $key ?>">
+                <input type="checkbox" name="symptoms[]" id="<?php echo $key ?>">
+                <?php echo $value ?>
+              </label>
+            </div>
+          <?php } ?>
+        </div>
       </div>
-      <div class="submit">
+      <div class="content">
         <input type="submit">
         <input type="text">
       </div>
--- home.php
+++ home.php
@@ -17,7 +17,83 @@ ob_start();
 </head>
 
 <body>
-<?php include "./layout/dash.php" ?>  
+ 
+<div class="container">
+    <div class="topbar">
+      <div class="logo">
+        <h2>wecare.</h2>
+      </div>
+
+      <!-- search bar start -->
+      <div class="search">
+        <input type="text" name="search" placeholder="search here">
+        <label for="search"><i class="fas fa-search"></i></label>
+      </div>
+      <!-- search bar end -->
+
+      <i class="fas fa-bell"></i>
+      <div class="user">
+        <?php $email = array_key_exists('username', $_SESSION) ? $_SESSION['username'] : false ?>
+        <?php echo "Welcome ". $_SESSION['username']?>
+      </div>
+    </div>
+
+    <!-- sidebar start -->
+    <div class="sidebar">
+      <ul>
+        <li>
+          <a href="/we_care/home.php#home">
+            <i class="fas fa-th-large"></i>
+            <div>Home</div>
+          </a>
+        </li>
+        <li>
+          <a href="/we_care/disease.php#disease">
+            <i class="fas fa-user-md"></i>
+            <div>Diseases Prediction system</div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fas fa-chalkboard-teacher"></i>
+            <div>Doctor Appointment</div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fas fa-users"></i>
+            <div>Blood Bank</div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fas fa-hospital"></i>
+            <div>Buy Medicine</div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fas fa-procedures"></i>
+            <div>Ambulance</div>
+          </a>
+        </li>
+        <li>
+          <a href="logout.php">
+            <i class="fas fa-cog"></i>
+            <div>Logout</div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fas fa-question"></i>
+            <div>Help</div>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- sidebar end -->
+    
+  
 <div class="main">
       <div class="cards">
         <div class="card">
